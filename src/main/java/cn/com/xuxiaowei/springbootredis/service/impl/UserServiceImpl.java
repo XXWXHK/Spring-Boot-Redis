def8355eaf114b43f904350f3d3aae1a63fec685
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 17:59:03 +0800

    插入一条记录（选择字段，策略插入，Redis）

--- src/main/java/cn/com/xuxiaowei/springbootredis/service/impl/UserServiceImpl.java
+++ src/main/java/cn/com/xuxiaowei/springbootredis/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package cn.com.xuxiaowei.springbootredis.service.impl;
 
 import cn.com.xuxiaowei.springbootredis.entity.User;
+import cn.com.xuxiaowei.springbootredis.framework.tool.SuperSqlHelper;
 import cn.com.xuxiaowei.springbootredis.mapper.UserMapper;
 import cn.com.xuxiaowei.springbootredis.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +18,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    /**
+     * 插入一条记录（选择字段，策略插入，Redis）
+     * <p>
+     * 无论如何都插入到数据并保存到Redis
+     * <p>
+     * value 放入Redis中的值
+     * key 实体类的主键（需要有唯一性）
+     *
+     * @param entity 要插入的实体对象
+     * @return 实体对象（插入结果返回了实体类主键）
+     */
+    @CachePut(value = "User", key = "#entity.id")
+    public User saveCachePut(User entity) {
+        int insert = super.baseMapper.insert(entity);
+        return SuperSqlHelper.retBool(insert, entity);
+    }
 
 }

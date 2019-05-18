9ea1379b41230862e72cd23ad3748f3d74626c4e
徐晓伟 <xuxiaowei@live.hk>
2019-05-18 17:35:57 +0800

    新建项目提交。

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Spring-Boot-Redis
 Spring Boot 整合 Redis。
+
+### Spring Boot 依赖
+    
+- Web
+    - Web                   使用Tomcat和Spring MVC进行全栈Web开发
+    
+- Template
+	- Thymeleaf             Thymeleaf模板引擎（页面）
+    
+- SQL
+    - MySQL                 MySQL JDBC驱动程序
+    
+- NoSQL
+    - Redis                 Redis键值数据存储，包括spring-data-redis
---
+++ src/main/java/cn/com/xuxiaowei/springbootredis/SpringBootRedisApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei.springbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行的入口
+ *
+ * @author xuxiaowei
+ */
+@SpringBootApplication
+public class SpringBootRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisApplication.class, args);
+    }
+
+}
